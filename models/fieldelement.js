388eda182d28fc5945e52f2828545b4b449f09c1
Nate Ashford <nate@technocrats2239.org>
2019-10-05 14:02:52 -0500

    Fixed association to Game

--- models/fieldelement.js
+++ models/fieldelement.js
@@ -2,13 +2,14 @@
 module.exports = (sequelize, DataTypes) => {
   const FieldElement = sequelize.define('FieldElement', {
     key: DataTypes.STRING,
+    year: DataTypes.INTEGER,
     name: DataTypes.STRING,
     description: DataTypes.STRING,
     outline: DataTypes.STRING
   }, {});
   FieldElement.associate = function(models) {
     // associations can be defined here
-    FieldElement.belongsTo(models.Game);
+    FieldElement.belongsTo(models.Game, { foreignKey: 'year' });
   };
   return FieldElement;
 };

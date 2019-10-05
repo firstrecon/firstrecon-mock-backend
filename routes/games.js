65343b107fda6364fb0ff1e8a405c3ed4140692d
Nate Ashford <nate@technocrats2239.org>
2019-10-05 11:56:47 -0500

    GET Game by year works

--- routes/games.js
+++ routes/games.js
@@ -3,10 +3,17 @@ const router = express.Router();
 const { Game } = require('../models');
 
 
-/* GET users listing. */
+/* GET games listing. */
 router.get('/', async (req, res, next) => {
   const games = await Game.findAll()
   res.json({ games });
 });
+
+// GET game by year
+router.get('/:year', async (req, res, next) => {
+  const game = await Game.findByPk(req.params.year);
+  res.json({ game });
+});
+
 
 module.exports = router;
--- test/gamesRouteSpec.js
+++ test/gamesRouteSpec.js
@@ -21,13 +21,32 @@ describe("games route", () => {
 
                     games.forEach( game => {
                         expect(game.year).to.exist;
+                        expect(game.year).to.be.a("Number");
                         expect(game.name).to.exist;
+                        expect(game.name).to.be.a("String");
                     });
                 });
         });
     });
 
     describe("GET /games/<year>", () => {
+        [2019, 2020].forEach( year => {
+            it(`should return the game for year ${year}`, () => {
+                request(app)
+                    .get(`/games/${year}`)
+                    .end((err, res) => {
+                        expect(res).to.have.status(200);
+                        expect(res).to.be.json;
+
+                        const game = res.body.game;
+                        expect(game).to.exist;
+                        expect(game.year).to.exist;
+                        expect(game.year).to.be.a("Number");
+                        expect(game.name).to.exist;
+                        expect(game.name).to.be.a("String");
+                    });
+            });
+        });
 
     });
 
